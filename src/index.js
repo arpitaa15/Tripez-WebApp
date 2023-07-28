6f61b02b59310636f40872385ed9d286b9817396
arpitaa15 <arpitapyne15@gmail.com>
2023-07-29 00:55:43 +0530

    Made few changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import About from './components/About/about';
+import Contact from './components/Contact/Contact';
+
+
+import{
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </React.StrictMode>
+  
 );
