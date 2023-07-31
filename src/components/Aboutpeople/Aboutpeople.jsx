66dab858d170a3fc2be875785b21cd30c7bc3192
arpitaa15 <arpitapyne15@gmail.com>
2023-07-31 21:29:08 +0530

    Made changes to the About page

--- src/components/Aboutpeople/Aboutpeople.jsx
+++ src/components/Aboutpeople/Aboutpeople.jsx
@@ -7,14 +7,14 @@ import img3 from '../../Assets/Kate.jpg'
 function Aboutpeople() {
   return (
 <section className='people'>
-<h2 className='title'>Meet Our Team</h2>
+<h2 className='team'>Meet Our Team</h2>
 <div className="row">
   <div className="column">
     <div className="card">
     <img src={img}/>
       <div className="container">
         <h2>Jane Doe</h2>
-        <p className="title">CEO</p>
+        <p className="designation">CEO</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>jane@example.com</p>
         <p><button className="button">Contact</button></p>
@@ -27,7 +27,7 @@ function Aboutpeople() {
     <img src={img2} />
       <div className="container">
         <h2>Mike Ross</h2>
-        <p className="title">Founder</p>
+        <p className="designation">Founder</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>mike@example.com</p>
         <p><button className="button">Contact</button></p>
@@ -40,7 +40,7 @@ function Aboutpeople() {
     <img src={img3} />
       <div className="container">
         <h2>Kate Williams</h2>
-        <p className="title">Director</p>
+        <p className="designation">Director</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>kate@example.com</p>
         <p><button className="button">Contact</button></p>
--- src/components/SignIn/signin.jsx
+++ src/components/SignIn/signin.jsx
@@ -82,7 +82,7 @@ function SignIn() {
     <section>
     <div className="app">
       <div className="login-form">
-        <div className="title">Sign In</div>
+        <div className="Signin">Sign In</div>
         {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
       </div>
     </div>
